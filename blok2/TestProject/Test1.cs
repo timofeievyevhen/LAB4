fefea738ce8192b0e3285127b9e8ab347b89bd54
timofeievyevhen <timofeiev.yevhen1125@vu.cdu.edu.ua>
2026-06-14 14:21:29 +0300

    Add files via upload

---
+++ blok2/TestProject/Test1.cs
@@ -0,0 +1,26 @@
+﻿using ConsoleApp8;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestProject
+{
+    [TestClass]
+    public class LabTests
+    {
+        [TestMethod]
+        public void Test_NormalGrade_ParsesCorrectly()
+        {
+            // Перевіряємо звичайну оцінку
+            double result = Program.ParseGradeForProcessing("5");
+            Assert.AreEqual(5.0, result);
+        }
+
+        [TestMethod]
+        public void Test_AbsentGrade_ReturnsTwo()
+        {
+            // Перевіряємо вимогу лаби: неявка "-" має ставати двійкою
+            double result = Program.ParseGradeForProcessing("-");
+            Assert.AreEqual(2.0, result);
+        }
+    }
+}
---
+++ blok2/TestProject1/Test1.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+// Підключаємо простір імен вашого головного проєкту, щоб тест бачив клас Program
+using StudentDataLab;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class StudentTests
+    {
+        [TestMethod]
+        public void Test_ParseGrade_NormalGrade_ReturnsSameNumber()
+        {
+            // Перевіряємо, чи звичайна текстова оцінка "5" правильно конвертується в число 5.0
+            double result = Program.ParseGradeForProcessing("5");
+
+            Assert.AreEqual(5.0, result);
+        }
+
+        [TestMethod]
+        public void Test_ParseGrade_AbsentSign_ReturnsTwo()
+        {
+            // Головна фішка лаби: перевіряємо, чи неявка "-" перетворюється на 2.0 під час обробки
+            double result = Program.ParseGradeForProcessing("-");
+
+            Assert.AreEqual(2.0, result);
+        }
+    }
+}
